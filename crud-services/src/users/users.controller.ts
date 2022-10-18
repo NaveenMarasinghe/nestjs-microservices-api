c86aa5b5527a70a8ce0cc6075edcab5320cb0738
NaveenMare <naveenmareftw@gmail.com>
2022-10-18 10:32:16 +0530

    Add users services

--- crud-services/src/users/users.controller.ts
+++ crud-services/src/users/users.controller.ts
@@ -1,9 +1,10 @@
 import { Controller } from '@nestjs/common';
-import { GrpcMethod, MessagePattern, Payload } from '@nestjs/microservices';
+import { GrpcMethod } from '@nestjs/microservices';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { Metadata, ServerUnaryCall } from '@grpc/grpc-js';
 import { UsersService } from './users.service';
+import { IUser } from './dto/IUser';
 
 export type UserById = {
   id: number;
@@ -13,29 +14,31 @@ export type UserById = {
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  // @MessagePattern('createUser')
-  // create(@Payload() createUserDto: CreateUserDto) {
-  //   return this.usersService.create(createUserDto);
-  // }
-
-  // @MessagePattern('findAllUsers')
-  // findAll() {
-  //   return this.usersService.findAll();
-  // }
-
   @GrpcMethod('CrudService', 'FindOneUser')
-  findOne(data: UserById, metadata: Metadata, call: ServerUnaryCall<any, any>) {
-    //return { id: 1, name: 'john' };
-    return this.usersService.findOne(data.id);
+  findOneUser(
+    data: UserById,
+    metadata: Metadata,
+    call: ServerUnaryCall<any, any>,
+  ) {
+    return this.usersService.findOneUser(data.id);
   }
 
-  // @MessagePattern('updateUser')
-  // update(@Payload() updateUserDto: UpdateUserDto) {
-  //   return this.usersService.update(updateUserDto.id, updateUserDto);
-  // }
+  @GrpcMethod('CrudService', 'AddNewUser')
+  addNewUser(data: IUser, metadata: Metadata, call: ServerUnaryCall<any, any>) {
+    return this.usersService.addNewUser(data);
+  }
 
-  // @MessagePattern('removeUser')
-  // remove(@Payload() id: number) {
-  //   return this.usersService.remove(id);
-  // }
+  @GrpcMethod('CrudService', 'UpdateUser')
+  updateUser(data: any, metadata: Metadata, call: ServerUnaryCall<any, any>) {
+    return this.usersService.updateUser(data.user, data.id);
+  }
+
+  @GrpcMethod('CrudService', 'DeleteUser')
+  deleteUser(
+    data: UserById,
+    metadata: Metadata,
+    call: ServerUnaryCall<any, any>,
+  ) {
+    return this.usersService.deleteUser(data.id);
+  }
 }
--- crud-services/src/users/users.service.ts
+++ crud-services/src/users/users.service.ts
@@ -1,26 +1,53 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
+import { IUser } from './dto/IUser';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+  async findAll(): Promise<User[]> {
+    return await this.usersRepository.find();
+  }
+  async findOneUser(id: number): Promise<User> {
+    const user = await this.usersRepository.findOne({
+      where: { id: id },
+    });
+    return user;
+  }
+  async addNewUser(data: IUser): Promise<User> {
+    const user = new User();
+    user.email = data.email;
+    user.name = data.name;
+    user.password = data.password;
+
+    return await this.usersRepository.save(user);
+  }
+  async updateUser(data: User, id: number): Promise<User> {
+    const result = await this.usersRepository
+      .createQueryBuilder()
+      .update({
+        email: data.email,
+        password: data.password,
+        name: data.name,
+      })
+      .where({
+        id: id,
+      })
+      .returning('*')
+      .execute();
+
+    return await result.raw[0];
   }
 
-  findAll() {
-    return `This action returns all users`;
-  }
-
-  findOne(id: number) {
-    return { id: 1, name: 'John' };
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async deleteUser(data: number) {
+    await this.usersRepository.delete({ id: data });
+    return { response: 'Success' };
   }
 }
